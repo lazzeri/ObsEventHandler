af8acd19346d37ed858b771320660ff976f68050
lazzeri <lucalazzeri97@gmail.com>
2019-11-26 09:43:40 +0100

    Added Event Handling for Invites and Fanning, Moments missing

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,20 @@
 var broadcastId;
 var userId;
 var self = this;
-var newFans;
-var eventsToTrigger;
-var userName = "";
+this.peerId = uuidv4();
+this.signalingWS = null;
 
-function RunCode()
+var newFans = [];
+var newInvites = [];
+
+var eventsToTrigger = [];
+
+
+var userName = "DashPena";
+
+async function FetchBroadcastId()
 {
-    if(FetchBroadcastId() == 1)
-        FetchData();
-
-    //Loop here to establish connection and to be shure that stream is running
-}
-
-function FetchBroadcastId()
-{
+    console.log("Fetching....");
     var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
     targetUrl = 'https://api.younow.com/php/api/broadcast/info/curId=0/user=' + userName;
     var json = fetch(proxyUrl + targetUrl)
@@ -25,16 +25,26 @@ function FetchBroadcastId()
         if(json.length < 1)
         {
             console.log("No Data Found");
+            return;
+            //TODO Wait for a while and retry
         }
         else
         {
+            if(done.errorCode != 0)
+            {
+                console.log("User not online or not found");
+                //Wait for a while and retry
+            }
+
             userId = done.userId;
             broadcastId = done.broadcastId;
-            return 1;
+            console.log("Data Found");
+            FetchData();
+            return;
         }
   })
   .catch(e => {
-    console.log("Error with download");
+    console.log(e);
     return e;
   });
 }
@@ -43,24 +53,23 @@ function FetchBroadcastId()
 function FetchData()
 {
 	var patt = /w3schools/i;
+    console.log("Connecting WebSocket");
+
 
 	//Find broadcastId somehow, could download json and convert
 	
 	//First Startup Connection:
-
-	this.peerId = uuidv4();
-	this.signalingWS = null;
 	this.signalingWS = new WebSocket('wss://signaling.younow-prod.video.propsproject.com/?roomId=' + broadcastId + '&isHost=false&peerId=' + self.peerId);
 
 	var pusher = new Pusher('d5b7447226fc2cd78dbb', {
         cluster: "younow"
     });
     var channel = pusher.subscribe("public-channel_" + userId);
-    var self = this;
 
 
     //For Single likes
     channel.bind('onLikes', function(data) {
+        console.log("TESTLIKE")
         console.log("1 Like");
     });
 
@@ -68,34 +77,74 @@ function FetchData()
     channel.bind('onChat', function(data) {
         for (i = 0; i < data.message.comments.length; i++)
         {
-        	var input = data.message.comments[i].name + "    " + data.message.comments[i].comment, '';
-        	
+        	var input = data.message.comments[i].name + "    " + data.message.comments[i].comment;
+
+            
+            //Doesnt include becoming fan of guest
         	if(input.includes("I became a fan!"))
         	{
         		var found = false;
 
-        		for each (var name in newFans) 
+        		for (var name in newFans) 
         		{
   					if(name.localeCompare(data.message.comments[i].name) == 0);
-  						found = true;
+  						{
+                            found = true;
+                            console.log("Replica found for fanning found:" + data.message.comments[i].name);
+                        }
 				}
-				//new fan
 				if(!found)
 				{
 					newFans.push(data.message.comments[i].name);
-					var newEvent = new Event("fans",data.message.comments[i].name,"","");
-					eventsToTrigger.push(Event);					 	
+					var newEvent = new Event("fans",data.message.comments[i].name,data.message.comments[i].userId,"");
+					eventsToTrigger.push(newEvent);	
+                    console.log("New Event for Fanning added");				 	
 				}
         	}
+
+            if(input.includes("invited") && input.includes("fans to this broadcast."))
+            {
+                var found = false;
+                var matches_array = input.match(/(\d+)/); 
+
+                if(matches_array.length == 2)
+                {
+                    for (var name in newInvites) 
+                    {
+                        if(name.localeCompare(data.message.comments[i].name) == 0);
+                        {
+                            found = true;
+                            console.log("Replica found for invite found:" + data.message.comments[i].name);
+                        }
+                    }
+                    if(!found)
+                    {
+                        console.log("Found invite val = " + matches_array[0]);
+                        newInvites.push(data.message.comments[i].name);
+                        var newEvent = new Event("invites",data.message.comments[i].name,data.message.comments[i].userId,"");
+                        eventsToTrigger.push(newEvent); 
+                        console.log("New Event for Inivte added");                 
+                    }
+                }
+            }
+
+            //Get Moments
+
+
         } 
     });
 
     //Get Gifts
     channel.bind('onGift', function(data) {
-        for (i = 0; i < data.message.gifts.length; i++)
+        console.log("TESTGIFT");
+        if(data.message != "undefined")
         {
-            console.log(data.message.gifts[i].giftId);    
+             for (i = 0; i < data.message.gifts.length; i++)
+            {
+                console.log(data.message.gifts[i].giftId);    
+            }   
         }
+       
     });
 }
 
@@ -107,29 +156,24 @@ function uuidv4() {
     });
 }
 
-
-//Create Regex for both
-//I became a fan!
-
-var fanpat  = /I became a fan!/i;
-var test = "I became a fan!"
-console.log(test.includes("I became a fan!"));
-
-
-//Create queue + for invites and fans make a list of users
-
-
-//Iterate trough queue with switch(category name, string image source, string name,string inviteamount)
-
 class Event {
-  constructor(category,name,image,inviteVal) {
+  constructor(category,name,id,inviteVal) {
     this.category = category;
     this.name = name;
-    this.image = image;
+    this.id = id;
     this.inviteVal = inviteVal;
 
   }
 }
+
+function Sleep(milliseconds) {
+   return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+
+//Create queue + for invites and fans make a list of users
+
+//Iterate trough queue with switch(category name, string id , string name,string inviteamount)
 
 
 //Next steps create Animation elements for each one and put all in one big animaiton 
@@ -138,3 +182,7 @@ class Event {
 //Need regex for capturing
 //Need regex for invites + amount of invites //Cache invite
 
+ function RunCode()
+{
+     FetchBroadcastId();
+}
